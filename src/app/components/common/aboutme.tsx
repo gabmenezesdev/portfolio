04fe3d0792e0f9155bb4d30d438c06433a73512e
Gabriel dos Santos <gabrielsilva.mene@gmail.com>
2024-10-06 20:30:42 -0300

    feat: new about me

--- src/app/components/common/aboutme.tsx
+++ src/app/components/common/aboutme.tsx
@@ -1,14 +1,15 @@
 export default function AboutMe(){
 
     return (
-        <div>
-            <p>
-            I'm Gabriel, a full-stack software engineer with around 4 years of experience building and maintaining scalable applications. My expertise lies in backend development with Node.js, though I have extensive experience in frontend technologies like React and Angular. I have worked on various projects, from networking and chatbot development to financial dashboards, always aiming to deliver high-quality solutions that provide real value.
-            <br/>
-            Currently, I'm honing my skills as a senior developer at Medisigh, where I focus on enhancing user experience and optimizing performance using techniques like lazy loading, caching, and pagination. I enjoy collaborating with small, nimble teams and am always looking to improve through teamwork and constant learning.
-            <br/>
-            I'm passionate about creating efficient solutions and being a reliable partner for my team. Whether it's designing new features or maintaining legacy systems, my goal is to help build technology that truly makes an impact.
-            </p>
-        </div>
+<div>
+    <p>
+    I'm Gabriel, a full-stack software engineer with approximately 4 years of experience in building and maintaining scalable applications. My core expertise is in backend development with Node.js, but I also have significant experience with frontend technologies like React and Angular. Over the years, I’ve worked on diverse projects, including networking solutions, chatbot development, and financial dashboards, always aiming to deliver high-quality, value-driven solutions.
+    <br/><br/>
+    Currently, as a senior developer at Medisigh, I'm focused on optimizing user experience and application performance by leveraging techniques like lazy loading, caching, and pagination. I thrive in small, nimble teams where collaboration and constant learning are key, always seeking ways to grow and improve both individually and as part of a team.
+    <br/><br/>
+    I'm passionate about creating efficient, impactful solutions and being a dependable partner for my team. Whether developing new features or maintaining legacy systems, my goal is to build technology that truly makes a difference.
+    </p>
+</div>
+
     )
 }
