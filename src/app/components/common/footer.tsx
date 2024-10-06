df20edca2631fddf49e9f68703b3c7c352551ef8
Gabriel dos Santos <gabrielsilva.mene@gmail.com>
2024-10-05 21:45:26 -0300

    feat: update links

--- src/app/components/common/footer.tsx
+++ src/app/components/common/footer.tsx
@@ -9,16 +9,13 @@ export default function Footer(){
                 </span>
                 <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
                     <li>
-                        <a href="#" className="hover:underline me-4 md:me-6">Experience</a>
+                        <a href="#experiences" className="hover:underline me-4 md:me-6">Experience</a>
                     </li>
                     <li>
-                        <a href="#" className="hover:underline me-4 md:me-6">About me</a>
+                        <a href="#about-me" className="hover:underline me-4 md:me-6">About me</a>
                     </li>
                     <li>
                         <a href="#" className="hover:underline me-4 md:me-6">Contact</a>
-                    </li>
-                    <li>
-                        <a href="#" className="hover:underline">Contact</a>
                     </li>
                 </ul>
                 </div>
